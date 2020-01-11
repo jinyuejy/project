a6967bb336e22e30996c942b65ed33e7247832b8
jinyuejy <1131115090@qq.com>
2020-01-11 22:29:09 +0800

    commit on master

--- database/registered.py
+++ database/registered.py
@@ -4,43 +4,43 @@ t=time()
 dsn='host=localhost dbname=csdb user=hopers password=hope'
 file='information.xlsx'
 
-# sql='''
-# insert 
-# into student
-# values(%s,%s,%s,%s,%s)
-# '''
-# submitdata.submit(dsn,sql,file,0,None)
+sql='''
+insert 
+into student
+values(%s,%s,%s,%s,%s)
+'''
+submitdata.submit(dsn,sql,file,0,None)
 
-#   ##学生信息录入
+  ##学生信息录入
 
 
-# sql='''
-# insert 
-# into course
-# values(%s,%s,%s,%s,%s)
-# '''
-# submitdata.submit(dsn,sql,file,2,3)
+sql='''
+insert 
+into course
+values(%s,%s,%s,%s,%s)
+'''
+submitdata.submit(dsn,sql,file,2,3)
 
-# # 课程信息录入
+# 课程信息录入
 
-# sql='''
-# insert
-# into grade
-# values(%s,%s,%s)
-# '''
-# submitdata.submit(dsn,sql,file,1,2)
+sql='''
+insert
+into grade
+values(%s,%s,%s)
+'''
+submitdata.submit(dsn,sql,file,1,2)
 
-# #成绩信息录入
+#成绩信息录入
 
-# sql='''
-# insert
-# into course_time
-# values(%s,%s,%s,%s,%s)
-# '''
+sql='''
+insert
+into course_time
+values(%s,%s,%s,%s,%s)
+'''
 
-# submitdata.submit(dsn,sql,file,3)
+submitdata.submit(dsn,sql,file,3)
 
-# # 课程时间以及位置信息
+# 课程时间以及位置信息
 
 sql='''
 insert
